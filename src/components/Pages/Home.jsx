184828b859d4842e24a9cb409cd679d6f9c45f57
reyesro <ronald.reyes@dis.tech>
2025-06-01 18:53:51 +0800

    grammar update

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -6,7 +6,7 @@ import SkillFrames from "../SkillFrames";
 const Home = () => {
   return (
     <div style={{ position: "relative", marginTop: "0px", fontSize: "15px"}} className="page about" >
-      <h1 className="Home-Header" style={{marginTop:"6rem"}}>Build Systems That Works</h1>
+      <h1 className="Home-Header" style={{marginTop:"6rem"}}>Build Systems That Work</h1>
       <Hero/>
 
       <SkillFrames/>
